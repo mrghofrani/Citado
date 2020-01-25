21575a43b91f329480fbed78f1ee1cac04846f34
Mohammadreza Ghofrani <mohammadrezaghofrany@gmial.com>
2020-01-25 20:18:10 +0330

    Insertion of address has been completed

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
 from kivy.lang import Builder
 
+from datetime import datetime
+
 Builder.load_file('insert_customer.kv')
+Builder.load_file('insert_biker.kv')
+Builder.load_file('insert_address.kv')
 
-global db_connection
 
-def initialize():
-    global db_connection
-    db_connection = psycopg2.connect(user="engmrgh",
+def get_connection():
+    return psycopg2.connect(user="engmrgh",
                                     password="h3ll9db",
                                     host="localhost",
                                     port="5432",
@@ -27,6 +29,12 @@ class MainLayout(BoxLayout):
 
     def insert_customer_button(self):
         show_insert_customer_popup()
+
+    def insert_biker_button(self):
+        show_insert_biker_popup()
+
+    def insert_address_button(self):
+        show_insert_address_popup()
 
 
 class FoodOrderPopUp(BoxLayout):
@@ -44,25 +52,81 @@ class InsertCustomerPopUp(BoxLayout):
         last_name = self.ids.customerLastName.text
         mobile_number = self.ids.customerMobileNumber.text
         age = self.ids.customerAge.text
+        connection = get_connection()
         postgres_insert_query = " INSERT INTO customer(national_code, first_name, last_name, mobile_number, age) " \
-                                "VALUES (%s, %s, %s, %s, %s) "
+                                " VALUES (%s, %s, %s, %s, %s) "
         values = (national_code, first_name, last_name, mobile_number, age)
         try:
-            c = db_connection.cursor()
-            c.execute(postgres_insert_query, values)
+            cursor = connection.cursor()
+            cursor.execute(postgres_insert_query, values)
 
-            db_connection.commit()
-            count = c.rowcount
+            connection.commit()
+            count = cursor.rowcount
             print(count, "Record inserted successfully into mobile table")
 
         except (Exception, psycopg2.Error) as error:
-            if db_connection:
+            if connection:
                 print("Failed to insert record into mobile table", error)
         finally:
             # closing database connection.
-            if (db_connection):
-                c.close()
+            if connection :
+                cursor.close()
+                connection.close()
 
+
+class InsertBikerPopUp(BoxLayout):
+    def submit(self):
+        national_code = self.ids.bikerNationalCodeInput.text
+        first_name = self.ids.bikerFirstName.text
+        last_name = self.ids.bikerLastName.text
+        mobile_number = self.ids.bikerMobileNumber.text
+        connection = get_connection()
+        postgres_insert_query = " INSERT INTO bike_delivery(national_code, first_name, last_name, mobile_number) " \
+                                "VALUES (%s, %s, %s, %s) "
+        values = (national_code, first_name, last_name, mobile_number)
+        try:
+            cursor = connection.cursor()
+            cursor.execute(postgres_insert_query, values)
+
+            connection.commit()
+            count = cursor.rowcount
+            print(count, "Record inserted successfully into bike_delivery table")
+
+        except (Exception, psycopg2.Error) as error:
+            if connection:
+                print("Failed to insert record into bike_delivery table", error)
+        finally:
+            # closing database connection.
+            if connection:
+                cursor.close()
+                connection.close()
+
+
+class InsertAddressPopUp(BoxLayout):
+    def submit(self):
+        phone_number = self.ids.addressPhoneNumber.text
+        name = self.ids.addressName.text
+        address = self.ids.addressAddress.text
+        connection = get_connection()
+        postgres_insert_query = " INSERT INTO address(phone, name, address) VALUES (%s,%s,%s) "
+
+        values = (phone_number, name, address)
+        try:
+            cursor = connection.cursor()
+            cursor.execute(postgres_insert_query, values)
+
+            connection.commit()
+            count = cursor.rowcount
+            print(count, "Record inserted successfully into address table")
+
+        except (Exception, psycopg2.Error) as error:
+            if connection:
+                print("Failed to insert record into address table ", error)
+        finally:
+            # closing database connection.
+            if connection:
+                cursor.close()
+                connection.close()
 
 
 class MainPage(App):
@@ -91,6 +155,19 @@ def show_insert_customer_popup():
     popup_window.open()
 
 
+def show_insert_biker_popup():
+    show = InsertBikerPopUp()
+    popup_window = Popup(title="Insert biker", content=show)
+    show.ids['insertBikerCancel'].bind(on_press=popup_window.dismiss)
+    popup_window.open()
+
+
+def show_insert_address_popup():
+    show = InsertAddressPopUp()
+    popup_window = Popup(title="Insert Address", content=show)
+    show.ids['insertAddressCancel'].bind(on_press=popup_window.dismiss)
+    popup_window.open()
+
+
 if __name__ == "__main__":
-    initialize()
     MainPage().run()
